0017bc0d4b761ff3e7a642cf25e6dbe77b99309c
Lefebvre Steven <133742833+HamelenYakkington@users.noreply.github.com>
2024-10-07 13:55:31 +0200

    docker
    Utilisation de Docker pour configurer l'environnement du projet.
    make start (pour lancer les conteneurs)
    puis
    make build (pour compiler le projet)
    
    make build crée un conteneur, le compile puis le supprime. Si une erreur survient, il faudra supprimer manuellement le conteneur de Maven. Une solution automatique sera implémentée plus tard.

---
+++ _00_ASBank2023/Docker/scriptSql/init.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE IF NOT EXISTS allard19u_coa_banque;
+USE allard19u_coa_banque;
+
+source /script/dumpSQL.sql;
+
+
+CREATE DATABASE IF NOT EXISTS allard19u_coa_banque_essais;
+USE allard19u_coa_banque_essais;
+
+source /script/dumpSQL_JUnitTest.sql;
