a31c3818d78a8c509d734693becb751ffbf6e4e3
Ydemae <113693952+Ydemae@users.noreply.github.com>
2024-10-14 16:32:45 +0200

    Tests Banque terminés
    Le % de coverage des méthodes n'est que de 81% car les getter/setter n'ont pas été testés étant donné qu'ils ne contiennent pas de logique particulière.

--- _00_ASBank2023/src/test/java/com/iut/banque/modele/TestsBanque.java
+++ _00_ASBank2023/src/test/java/com/iut/banque/modele/TestsBanque.java
@@ -13,42 +13,6 @@ public class TestsBanque {
 
     Banque banque;
 
-    @Test
-    public void testSetClients() {
-        banque = new Banque();
-        Map<String, Client> listeClients = new HashMap<String, Client>();
-
-        if (banque.getClients() != null){
-            Assert.fail("La liste des clients de la Banque est initialisée à la création d'une nouvelle banque");
-        }
-
-        Client testClient = new Client();
-        testClient.setNom("Fransisco Testeur");
-
-        Client testClient2 = new Client();
-        testClient2.setNom("Francis Testeur");
-
-        Client testClient3 = new Client();
-        testClient3.setNom("Francis Testeur");
-
-        listeClients.put("t.test", testClient);
-        listeClients.put("t.test2", testClient2);
-        listeClients.put("t.test3", testClient3);
-
-        banque.setClients(listeClients);
-
-        Map<String, Client> returnedListeClients = banque.getClients();
-
-        Assert.assertNotNull(returnedListeClients);
-
-        if (returnedListeClients.size() != listeClients.size()){
-            Assert.fail("La taille de la liste des clients est incorrecte");
-        }
-        if (returnedListeClients.get("t.test") != testClient){
-            Assert.fail("Le client renvoyé n'est pas égal au client passé à la banque");
-        }
-    }
-
     //Tests débiter
 
     @Test
@@ -102,8 +66,80 @@ public class TestsBanque {
 
     //Tests créditer
 
+    @Test
+    public void TestCrediterCompte() throws IllegalFormatException {
+        banque = new Banque();
+
+        Compte testCompte = new CompteSansDecouvert("WU1234567890", 0, new Client());
+
+        banque.crediter(testCompte, 100);
+
+
+        if (testCompte.getSolde() !=  100){
+            Assert.fail("Le compte n'a pas été crédité correctement");
+        }
+    }
+
     //Tests deleteUser
 
+    @Test
+    public void testDeleteUser() {
+        banque = new Banque();
+        Map<String, Client> listeClients = new HashMap<String, Client>();
+
+        if (banque.getClients() != null){
+            Assert.fail("La liste des clients de la Banque est initialisée à la création d'une nouvelle banque");
+        }
+
+        Client testClient = new Client();
+        testClient.setNom("Fransisco Testeur");
+
+        Client testClient2 = new Client();
+        testClient2.setNom("Francis Testeur");
+
+        Client testClient3 = new Client();
+        testClient3.setNom("Francis Testeur");
+
+        listeClients.put("t.test", testClient);
+        listeClients.put("t.test2", testClient2);
+        listeClients.put("t.test3", testClient3);
+
+        banque.setClients(listeClients);
+
+        Map<String, Client> returnedListeClients = banque.getClients();
+
+        Assert.assertNotNull(returnedListeClients);
+
+        if (returnedListeClients.size() != listeClients.size()){
+            Assert.fail("La taille de la liste des clients est incorrecte");
+        }
+        if (returnedListeClients.get("t.test") != testClient){
+            Assert.fail("Le client renvoyé n'est pas égal au client passé à la banque");
+        }
+
+        banque.deleteUser("t.test2");
+        returnedListeClients = banque.getClients();
+
+        if (returnedListeClients.get("t.test2") != null){
+            Assert.fail("La suppression du client de la banque n'a pas été effectué");
+        }
+
+    }
+
     //Tests changeDecouvert
+
+    @Test
+    public void TestChangeDecouvert() throws IllegalOperationException, IllegalFormatException {
+        banque = new Banque();
+
+        CompteAvecDecouvert testCompte = new CompteAvecDecouvert("WU1234567890", 0, 1, new Client());
+        banque.changeDecouvert(testCompte, 100);
+        try {
+            banque.debiter(testCompte, 100);
+        }
+        catch (InsufficientFundsException e){
+            Assert.fail("Le changement du découvert ne s'est pas effectué");
+        }
+    }
 
 }
